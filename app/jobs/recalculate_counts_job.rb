d23768304861feecd9f037c321cdee47f118806b
= <=>
2024-05-21 18:22:28 +0300

    remove unused code

--- app/jobs/recalculate_counts_job.rb
+++ app/jobs/recalculate_counts_job.rb
@@ -1,16 +1,3 @@
-# class RecalculateCountsJob < ApplicationJob
-#       queue_as :default
-
-#       def perform
-#         Application.find_each do |application|
-#           application.update(chats_count: application.chats.count)
-#           application.chats.find_each do |chat|
-#             chat.update(messages_count: chat.messages.count)
-#           end
-#         end
-#       end
-# end
-
 class RecalculateCountsJob
       include Sidekiq::Job
     
